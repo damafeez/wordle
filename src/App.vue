2642e742840eb9b4293ffb8a2a47e01b188a77ee
Afeez Awoyemi <damafeez@gmail.com>
2023-01-25 22:19:01 +0100

    feat: implement game over

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ const inputs = reactive(Array.from({ length: numRows }, () => ''))
 // currentRow can be 1 more than available rows
 // currentRow, apart from denoting the 'current row', also denotes that previous rows have been checked
 const currentRow = ref(0)
-const won = ref(false)
+const gameState = ref('playing')
 
 const rows = computed(() =>
   inputs.map((input, i) =>
@@ -23,14 +23,8 @@ const rows = computed(() =>
 )
 
 const onKeyDown = (e) => {
-  // won or no more row space or special input, disallow input
-  if (
-    won.value ||
-    currentRow.value >= numRows ||
-    e.ctrlKey ||
-    e.metaKey ||
-    e.shiftKey
-  ) {
+  // when game ends or special input was entered, disallow input
+  if (gameState.value !== 'playing' || e.ctrlKey || e.metaKey || e.shiftKey) {
     return
   }
   const inputLength = inputs[currentRow.value].length
@@ -41,11 +35,15 @@ const onKeyDown = (e) => {
   } else if (inputLength === numColumns && e.key === 'Enter') {
     if (words.includes(inputs[currentRow.value])) {
       if (inputs[currentRow.value] === correctWord) {
-        won.value = true
+        gameState.value = 'won'
         nextTick().then(() => alert("Hurray! You're a winner."))
       }
 
       currentRow.value++
+      if (currentRow.value >= numRows && gameState.value === 'playing') {
+        gameState.value = 'lost'
+        alert('You lost :(')
+      }
     } else alert('Word not found.')
   } else if (inputLength && e.key === 'Backspace') {
     inputs[currentRow.value] = inputs[currentRow.value].slice(0, -1)
